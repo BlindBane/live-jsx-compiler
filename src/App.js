4e43ed770695da6e88c738b7acc6d51c1e047e47
Aidan Bane <aidanfbane@gmail.com>
2017-08-13 17:42:27 -0700

    (refactor) Hello World Class Component (sans JSX) Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+// class App extends React.Component {
+//   render () {
+//     return <h1>Hello World!</h1>
+//   }
+// }
+
 class App extends React.Component {
   render () {
-    return <h1>Hello World!</h1>
+    return React.createElement('h1', null, 'Hello World!')
   }
 }
 
