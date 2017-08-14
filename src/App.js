3608dbe2ae29556184750edbe65605fa5cc1bcac
Aidan Bane <aidanfbane@gmail.com>
2017-08-13 17:49:44 -0700

    (doc) Added comments to previous component examples

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
 
+class App extends React.Component {
+  render () {
+    return <h1>Hello World!</h1>
+  }
+}
+
+export default App
+
+// Class Component ( w/ JSX )
 // class App extends React.Component {
 //   render () {
 //     return <h1>Hello World!</h1>
 //   }
 // }
 
+// Class Component ( sans-JSX )
 // class App extends React.Component {
 //   render () {
 //     return React.createElement('h1', null, 'Hello World!')
 //   }
 // }
 
-const App = () => <h1>Hello World!</h1>
-
-export default App
+// Stateless functional component
+// const App = () => <h1>Hello World!</h1>
