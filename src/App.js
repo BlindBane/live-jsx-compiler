f92125ec383b939a5500e5934acb2c1991f5a9ef
Aidan Bane <aidanfbane@gmail.com>
2017-08-13 18:04:15 -0700

    (doc) Added comment explaining render method error
    React's render method can only return one value.

--- src/App.js
+++ src/App.js
@@ -24,3 +24,30 @@ export default App
 
 // Stateless functional component
 // const App = () => <h1>Hello World!</h1>
+
+/* A react components render method can only return a single node.
+
+  class App extends React.Component {
+    render () {
+      return <h1>Hello Earth!</h1> <h1>Hello Mars!</h1>
+    }
+  }
+
+   For example, the class component above will return an error message like:
+
+     Syntax error: Adjacent JSX elements must be wrapped in an enclosing tag
+
+  Why can the render method only contain a single node?
+  Consider the following class component.  It should render the same output as the example above,
+  but it does not use JSX.
+
+  class App extends React.Component {
+    render () {
+      return React.createElement('h1', null, 'Hello Earth!') React.createElement('h1', null, 'Hello Mars!')
+    }
+  }
+
+  Javascript's return statement only returns one value.
+  JSX makes it difficult to see that render is returning more than one value if
+  all elements are not wrapped inside of one element.
+*/
