c6aee9bce70990f4201af4a31663eca699eae1c4
Aidan Bane <aidanfbane@gmail.com>
2017-08-13 17:45:51 -0700

    (refactor) Hello World Stateless Functional Component Added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import React from 'react'
 //   }
 // }
 
-class App extends React.Component {
-  render () {
-    return React.createElement('h1', null, 'Hello World!')
-  }
-}
+// class App extends React.Component {
+//   render () {
+//     return React.createElement('h1', null, 'Hello World!')
+//   }
+// }
+
+const App = () => <h1>Hello World!</h1>
 
 export default App
